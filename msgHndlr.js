9ceae7ef03090be1cecc5317db5f0712cad6caa4
Syahrul_Rahmadan (Arul_Ganz) <88493207+arulganz@users.noreply.github.com>
2021-10-29 14:05:37 +0700

    Update msgHndlr.js

--- msgHndlr.js
+++ msgHndlr.js
@@ -26,7 +26,7 @@ const msga = (message) => {
 /**DATABASE*/
 var presen = false
 var pren = 'composing'
-var namabot = 'BennyBOT'
+var namabot = 'ArulBotz'
 
 module.exports = benny = async(benny, ben) => {
 	try {
